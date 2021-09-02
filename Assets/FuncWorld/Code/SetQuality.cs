16e3a0704963f9d49d81f7d966ddaec248d7469a
Pema Malling <pemamalling@gmail.com>
2021-09-02 13:07:09 +0200

    Move around stuff, add quality

---
+++ Assets/FuncWorld/Code/SetQuality.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SetQuality : UdonSharpBehaviour
+{
+    public Mesh mesh;
+    public MeshFilter renderer;
+
+    public override void Interact()
+    {
+        renderer.mesh = mesh;
+    }
+}
