674d865c5135c06b1c665d2e5578ea41cf69701f
Pema Malling <pemamalling@gmail.com>
2021-09-07 18:55:43 +0200

    Sync code

---
+++ Assets/FuncWorld/Code/SyncCode.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class SyncCode : UdonSharpBehaviour
+{
+    public InputField input;
+    public Toggle toggle;
+    public Compiler compiler;
+
+    [UdonSynced] public string code = "";
+
+    public override void OnDeserialization()
+    {
+        if (toggle.isOn)
+        {
+            input.text = code;
+            compiler.Compile();
+        }
+    }
+
+    public void PerformSync()
+    {
+        if (toggle.isOn)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            code = input.text;
+            compiler.Compile();
+            RequestSerialization();
+        }
+    }
+}
